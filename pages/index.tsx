fb0471a2b4a5b8d9498c19c6cfe13b58f5a6f4b4
Alex Young <alex.young@ibm.com>
2024-03-12 14:20:04 -0500

    Update dependencies and remove unused files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import BlackBoxShell from '@/components/AppShell/AppShell';
 
 export default function HomePage() {
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <BlackBoxShell />
     </>
   );
 }
